ee40afe11dd762a52a6ade4847c970c33c38feda
Lucas Oliveira <lucascoliveira819@gmail.com>
2023-10-05 18:04:39 -0400

    feat: add initial registers on db

--- apps/api/src/app.ts
+++ apps/api/src/app.ts
@@ -1,6 +1,27 @@
 import express, { NextFunction, Request, Response } from "express";
 import cors from "cors";
+import { PrismaClient } from '@prisma/client'
 
 export const app = express();
 app.use(cors({ origin: true }))
 app.use(express.json());
+
+const prisma = new PrismaClient()
+
+prisma.task.createMany({
+  data: [
+    {
+      title: "Learn React",
+      done: true,
+    },
+    {
+      title: "Study NodeJS",
+      done: false,
+    },
+    {
+      title: "Pratice Typescript",
+      done: false,
+    },
+  ]
+})
+
