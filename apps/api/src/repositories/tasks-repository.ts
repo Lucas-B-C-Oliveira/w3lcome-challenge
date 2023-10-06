d9bbf0e139a8e5aba99dc5341282b2ad1b0e2886
Lucas Oliveira <lucascoliveira819@gmail.com>
2023-10-05 21:16:44 -0400

    refactor: change edit parameters type

--- apps/api/src/repositories/tasks-repository.ts
+++ apps/api/src/repositories/tasks-repository.ts
@@ -3,6 +3,6 @@ import { Prisma, Task } from '@prisma/client'
 export interface TasksRepository {
   findAll(limit?: number, offset?: number): Promise<Task[] | null>
   create(data: Prisma.TaskCreateInput): Promise<Task>
-  edit(id: number, newData: Task): Promise<Task>
+  edit(id: number, done: boolean): Promise<Task>
   delete(id: number): Promise<Task | null>
 }
