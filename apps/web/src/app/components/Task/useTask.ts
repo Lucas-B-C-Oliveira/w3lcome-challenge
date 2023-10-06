a073760ab36b5b06ad43abfb666d2d9af2bf1bb2
Lucas Oliveira <lucascoliveira819@gmail.com>
2023-10-06 03:03:16 -0400

    feat: add useTask

---
+++ apps/web/src/app/components/Task/useTask.ts
@@ -0,0 +1,13 @@
+import { TaskData } from "../../../@types"
+
+interface UseTaskProps {
+  taskData: TaskData
+}
+
+export function useTask({ taskData }: UseTaskProps) {
+
+  return {
+    title: taskData?.title,
+    done: taskData?.done
+  }
+}
