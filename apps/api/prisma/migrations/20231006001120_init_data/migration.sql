364c60003b638fe3bf36b94e1ae1fbfbc43d24df
Lucas Oliveira <lucascoliveira819@gmail.com>
2023-10-05 20:18:20 -0400

    feat: add initial data migration

---
+++ apps/api/prisma/migrations/20231006001120_init_data/migration.sql
@@ -0,0 +1,5 @@
+-- This is an empty migration.
+
+INSERT INTO tasks (title, done) VALUES ('Learn React', true);
+INSERT INTO tasks (title, done) VALUES ('Study NodeJS', false);
+INSERT INTO tasks (title, done) VALUES ('Pratice Typescript', false);
