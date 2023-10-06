88c990a1532a6f300a0a3f671b68334653d6532d
Lucas Oliveira <lucascoliveira819@gmail.com>
2023-10-05 21:33:03 -0400

    feat: add deleteTask route

--- apps/api/src/http/controllers/tasks/routes.ts
+++ apps/api/src/http/controllers/tasks/routes.ts
@@ -2,11 +2,13 @@ import { Router as expressRouter } from "express";
 import { getTasks } from "./getTasks";
 import { createTask } from "./createTask";
 import { updateTask } from "./updateTask";
+import { deleteTask } from "./deleteTask";
 
 const router = expressRouter();
 router.get("/", getTasks);
 router.post("/", createTask);
 router.patch("/", updateTask);
+router.delete("/", deleteTask);
 
 
 export const tasksRoutes = router;
