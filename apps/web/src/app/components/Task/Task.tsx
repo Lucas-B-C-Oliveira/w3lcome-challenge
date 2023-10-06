81bfe926960ab7c6ec1781274efde76fc105c09a
Lucas Oliveira <lucascoliveira819@gmail.com>
2023-10-06 05:23:28 -0400

    feat: add action buttons with modal

--- apps/web/src/app/components/Task/Task.tsx
+++ apps/web/src/app/components/Task/Task.tsx
@@ -1,8 +1,12 @@
+'use client'
 import { memo } from "react";
-import { CheckCircleIcon, ExclamationCircleIcon, PencilSquareIcon } from '@heroicons/react/20/solid'
+import { CheckCircleIcon, ExclamationCircleIcon, PencilSquareIcon, TrashIcon } from '@heroicons/react/20/solid'
 import { type TaskData } from "../../../@types";
 import { useTask } from "./useTask";
 import { ActionButton } from "../ActionButton/ActionButton";
+import { Container } from "../Modal/Container";
+import { DeleteTaskModal } from "../DeleteTaskModal/DeleteTaskModal";
+import { EditTaskModal } from "../EditTaskModal/EditTaskModal";
 
 interface TaskProps {
   taskData: TaskData
@@ -10,10 +14,10 @@ interface TaskProps {
 
 export const Task = memo(function Task({ taskData }: TaskProps) {
 
-  const { title, done } = useTask({ taskData })
+  const { title, done, id } = useTask({ taskData })
 
   return (
-    <div className="flex flex-row gap-1 items-center justify-between w-80">
+    <div className="flex flex-row gap-1 items-center justify-between w-96">
       <div className="flex flex-row gap-1 items-center">
 
 
@@ -31,10 +35,31 @@ export const Task = memo(function Task({ taskData }: TaskProps) {
 
       </div>
 
-      <ActionButton>
-        Edit
-        <PencilSquareIcon className="h-4 w-4 " />
-      </ActionButton>
+      <div className="flex flex-row gap-[0.05rem] items-center">
+
+        <Container openModalButton={
+          <ActionButton>
+            Edit
+            <PencilSquareIcon className="h-4 w-4 " />
+          </ActionButton>
+        }>
+
+          <EditTaskModal done={done} taskId={id} />
+
+        </Container>
+
+        <Container openModalButton={
+          <ActionButton
+            classNameToMerge={`bg-transparent hover:bg-transparent hover:text-red-400 text-red-500 shadow-none`}
+          >
+            <TrashIcon className="h-4 w-4 " />
+          </ActionButton>
+        }>
+
+          <DeleteTaskModal taskId={id} />
+
+        </Container>
+      </div>
 
     </div>
   )
