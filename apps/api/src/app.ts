f1d13f88a09f931f3f11db7c7d7b148af5cc66da
Lucas Oliveira <lucascoliveira819@gmail.com>
2023-10-05 20:18:51 -0400

    refactor: remove create initial data of code

--- apps/api/src/app.ts
+++ apps/api/src/app.ts
@@ -1,27 +1,13 @@
-import express, { NextFunction, Request, Response } from "express";
+import express from "express";
 import cors from "cors";
-import { PrismaClient } from '@prisma/client'
+import { tasksRoutes } from "./http/controllers/tasks/routes";
 
 export const app = express();
 app.use(cors({ origin: true }))
 app.use(express.json());
 
-const prisma = new PrismaClient()
+// Routes
 
-prisma.task.createMany({
-  data: [
-    {
-      title: "Learn React",
-      done: true,
-    },
-    {
-      title: "Study NodeJS",
-      done: false,
-    },
-    {
-      title: "Pratice Typescript",
-      done: false,
-    },
-  ]
-})
+// ### Tasks
+app.use("/tasks", tasksRoutes)
 
