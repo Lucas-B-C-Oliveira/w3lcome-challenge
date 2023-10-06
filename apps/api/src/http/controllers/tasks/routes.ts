7cdb1f61bdcffb27ecc2f04a3e23b772ee967ce5
Lucas Oliveira <lucascoliveira819@gmail.com>
2023-10-05 21:16:14 -0400

    feat: add updateTask route

--- apps/api/src/http/controllers/tasks/routes.ts
+++ apps/api/src/http/controllers/tasks/routes.ts
@@ -1,10 +1,12 @@
 import { Router as expressRouter } from "express";
 import { getTasks } from "./getTasks";
 import { createTask } from "./createTask";
+import { updateTask } from "./updateTask";
 
 const router = expressRouter();
 router.get("/", getTasks);
 router.post("/", createTask);
+router.patch("/", updateTask);
 
 
 export const tasksRoutes = router;
