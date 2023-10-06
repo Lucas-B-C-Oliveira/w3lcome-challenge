494a7319f43f71a67c8b74ac3936364f23f42ace
Lucas Oliveira <lucascoliveira819@gmail.com>
2023-10-06 05:58:57 -0400

    feat: add home page

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,8 +1,24 @@
+import { type Meta, type TaskData } from "../@types";
+import { fetcherAdapter } from "../utils/server/fetcherAdapter";
+import { TasksCard } from "./components/TasksCard/TasksCard";
 
-export default function Home(): JSX.Element {
+const LIMIT_DEFAULT = 5
+const OFFSET_DEFAULT = 0
+
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: { [key: string]: string | string[] | undefined }
+}) {
+
+
+  const limit = LIMIT_DEFAULT
+  const offset = searchParams?.offset ?? OFFSET_DEFAULT
+  const data = await fetcherAdapter<{ meta: Meta, tasks: TaskData[] }>(`tasks?limit=${limit}&offset=${offset}`)
+
   return (
-    <main className="w-full h-full bg-red-500">
-
+    <main id="main" className="w-full h-full bg-white flex items-center justify-center">
+      <TasksCard meta={data?.meta} tasks={data?.tasks} />
     </main>
   );
 }
