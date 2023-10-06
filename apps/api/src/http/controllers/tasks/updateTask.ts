5b85778ed13b962cb72223d274f20e3628eb1abb
Lucas Oliveira <lucascoliveira819@gmail.com>
2023-10-05 21:16:24 -0400

    feat: add updateTask endpoint

---
+++ apps/api/src/http/controllers/tasks/updateTask.ts
@@ -0,0 +1,36 @@
+import { ResourceNotFoundError } from "@/use-cases/errors/resource-not-found-error";
+import { makeUpdateTaskUseCase } from "@/use-cases/factories/make-update-task-use-case";
+import { Request, Response, NextFunction } from "express";
+import { z } from "zod";
+
+
+export async function updateTask(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+
+  const updateTaskBodySchema = z.object({
+    id: z.number(),
+    done: z.boolean()
+  })
+
+  const { id, done } = updateTaskBodySchema.parse(req.body)
+
+  try {
+    const updateTask = makeUpdateTaskUseCase();
+
+    const { task } = await updateTask.execute({
+      id,
+      done,
+    });
+
+    res.status(201).send(task);
+  } catch (error: any) {
+    if (error instanceof ResourceNotFoundError) {
+      res.status(404).send({ message: error?.message })
+    }
+    console.log("error", error);
+    next(error);
+  }
+};
