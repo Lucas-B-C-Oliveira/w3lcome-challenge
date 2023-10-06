b3e08b1f27bd3b8130f14ef9f26b7b3370a5784f
Lucas Oliveira <lucascoliveira819@gmail.com>
2023-10-05 20:55:25 -0400

    feat: createTask post route

--- apps/api/src/http/controllers/tasks/routes.ts
+++ apps/api/src/http/controllers/tasks/routes.ts
@@ -1,8 +1,10 @@
 import { Router as expressRouter } from "express";
 import { getTasks } from "./getTasks";
+import { createTask } from "./createTask";
 
 const router = expressRouter();
 router.get("/", getTasks);
+router.post("/", createTask);
 
 
 export const tasksRoutes = router;
