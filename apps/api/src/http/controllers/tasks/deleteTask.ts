f1fd477568c5d25688081ad47ae77ce61ddf6f23
Lucas Oliveira <lucascoliveira819@gmail.com>
2023-10-05 21:32:52 -0400

    feat: add deleteTask endpoint

---
+++ apps/api/src/http/controllers/tasks/deleteTask.ts
@@ -0,0 +1,37 @@
+import { ResourceNotFoundError } from "@/use-cases/errors/resource-not-found-error";
+import { makeDeleteTaskUseCase } from "@/use-cases/factories/make-delete-task-use-case";
+import { Request, Response, NextFunction } from "express";
+import { z } from "zod";
+
+
+export async function deleteTask(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  try {
+
+    const deleteTaskBodySchema = z.object({
+      id: z.number()
+    })
+
+    const { id } = deleteTaskBodySchema.parse(req.body)
+
+    const deleteTask = makeDeleteTaskUseCase();
+
+    const { task } = await deleteTask.execute({
+      id,
+    });
+
+    res.status(200).send({
+      message: `${task?.title} was deleted successfully`,
+      task,
+    });
+  } catch (error: any) {
+    if (error instanceof ResourceNotFoundError) {
+      res.status(404).send({ message: error?.message })
+    }
+    console.log("error", error);
+    next(error);
+  }
+};
